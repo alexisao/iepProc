a2817a92dcac9142b0fd73179e1bd8a1f9bf40d9
javilaotz <javilaortiz@gmail.com>
2017-06-22 15:15:15 -0500

    Ajustes para permitir al webmaster ver todas las pestañas

--- admin/php/get_sala_activa.php
+++ admin/php/get_sala_activa.php
@@ -27,26 +27,35 @@ if ($hora>="08:00:00" && $hora<="12:00:00") { $turno = 0;} #Prox a parametrizar
 if ($hora>="13:30:00" && $hora<="17:30:00") { $turno = 1;}
 if ($hora>="17:30:00" && $hora<="21:00:00") { $turno = 2;}
 
-#buscamos sala del usuario en ese día
-$qry_turno = "SELECT tu_espacio FROM tbl_turnos WHERE tu_us_id=".$user." AND tu_dia=".($day-1)." AND tu_turno=".$turno.";";
-$res_turno = mysql_query($qry_turno,$con->connect());
-if ($res_turno!=false) {
-	# code...
-	$row_turno = mysql_fetch_assoc($res_turno);
+#validamos si es superuser
+$s_tipo=$_SESSION["ses_tipo"];
+if($s_tipo != "1")
+{
+	#buscamos sala del usuario en ese día
+	$qry_turno = "SELECT tu_espacio FROM tbl_turnos WHERE tu_us_id=".$user." AND tu_dia=".($day-1)." AND tu_turno=".$turno.";";
+	$res_turno = mysql_query($qry_turno,$con->connect());
+	if ($res_turno!=false) {
+		# code...
+		$row_turno = mysql_fetch_assoc($res_turno);
 
-	if($row_turno!=false){
-		$value = $row_turno["tu_espacio"];
-		$res=true;
-		$mes="ok";
+		if($row_turno!=false){
+			$value = $row_turno["tu_espacio"];
+			$res=true;
+			$mes="ok";
+		}else{
+			$value = null;
+			$res=false;
+			$mes="Usted no tiene turnos programados en este momento";
+		}
 	}else{
 		$value = null;
 		$res=false;
 		$mes="Usted no tiene turnos programados en este momento";
 	}
 }else{
-	$value = null;
-	$res=false;
-	$mes="Usted no tiene turnos programados en este momento";
+	$value = "999";
+	$res=true;
+	$mes="Tipo de usuario: ".$s_tipo;
 }
 
 $response->res = $res;
